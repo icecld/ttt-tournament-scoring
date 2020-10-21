1c26d05ee4ff16dadbbb82c27c1912d0bc16cf35
icecld <73066816+icecld@users.noreply.github.com>
2020-10-21 18:49:01 +0100

    Changed locals to globals

--- lua/autorun/init.lua
+++ lua/autorun/init.lua
@@ -3,11 +3,12 @@
 -- Define scoring table
 -- ttt already defines SCORE let's use TOURNAMENT to avoid confusion in namespace
 TOURNAMENT = {}
+TOURNAMENT.TOTALROUNDS = 0
 
 -- Messing with the player metatable following ttt
 -- In here we extend the metatable to track round performance
-include("player_ext.lua")     -- Serverside 
-include("player_ext_shd.lua") -- Shared 
+include("player_ext.lua")     -- Serverside
+include("player_ext_shd.lua") -- Shared
 
 
 -- Round end handling, team scores etc.
